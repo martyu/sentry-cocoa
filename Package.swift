59f949c09d10d9ffc6659278d55cae4aee975e43
Marty Ulrich <marty.ulrich@care.com>
2024-11-20 17:44:29 -0800

    remove static lib

--- Package.swift
+++ Package.swift
@@ -5,31 +5,15 @@ let package = Package(
     name: "Sentry",
     platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
     products: [
-        .library(name: "Sentry", targets: ["Sentry"]),
-        .library(name: "Sentry-Dynamic", targets: ["Sentry-Dynamic"]),
-        .library(name: "SentrySwiftUI", targets: ["Sentry", "SentrySwiftUI"]),
-        .library(name: "SentrySwiftUI-Dynamic", targets: ["Sentry-Dynamic", "SentrySwiftUI-Dynamic"])
+        .library(name: "SentrySwiftUI", type: .dynamic, targets: ["Sentry-Dynamic", "SentrySwiftUI"]),
     ],
     targets: [
-        .binaryTarget(
-                    name: "Sentry",
-                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.40.1/Sentry.xcframework.zip",
-                    checksum: "db928e6fdc30de1aa97200576d86d467880df710cf5eeb76af23997968d7b2c7" //Sentry-Static
-                ),
         .binaryTarget(
                     name: "Sentry-Dynamic",
                     url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.40.1/Sentry-Dynamic.xcframework.zip",
                     checksum: "31c55833e848a27100d38b607867bb5ce197d7183303b2c4d8ba4fa97d288df3" //Sentry-Dynamic
                 ),
         .target ( name: "SentrySwiftUI",
-                  dependencies: ["Sentry", "SentryInternal"],
-                  path: "Sources/SentrySwiftUI",
-                  exclude: ["SentryInternal/", "module.modulemap"],
-                  linkerSettings: [
-                     .linkedFramework("Sentry")
-                  ]
-                ),
-        .target ( name: "SentrySwiftUI-Dynamic",
                   dependencies: ["Sentry-Dynamic", "SentryInternal"],
                   path: "Sources/SentrySwiftUI",
                   exclude: ["SentryInternal/", "module.modulemap"],
